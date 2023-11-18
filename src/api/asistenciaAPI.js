ed290b902a9d41e2b4adbbf4484823651cd6703b
alexis-moing <135295210+alexis-Moing@users.noreply.github.com>
2023-11-17 23:22:23 -0500

    probando dominio del back local

--- src/api/asistenciaAPI.js
+++ src/api/asistenciaAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const asistenciaApi = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://nfdfz6vz-8000.brs.devtunnels.ms",
 })
 
 export default asistenciaApi
